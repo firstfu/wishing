d1ce2d2bf345472b5622fda1feeed0ece78b9ca5
firstfu <firefirstfu@gmail.com>
2025-05-20 00:21:57 +0800

    移除 ESLint 配置中的不必要規則，簡化代碼檢查設定，以提升開發靈活性。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,8 +16,6 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "no-unused-expressions": "off",
     },
   },
 ];
